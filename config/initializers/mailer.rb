1e34a9933271a74a21ba2069536c758a8dd00eb3
Maxim Rykunov <maxim@rykunov.info>
2020-09-28 22:25:13 +0300

    Fix smtp

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,10 +1,12 @@
 ActionMailer::Base.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {
+  :tls => true,
   :address => "smtp.mail.ru",
   :port => 465,
-  :domain => '84962180218.ru',
+  :domain => 'mail.ru',
   :authentication => :plain,
   :user_name => 'liderstroy-dubna@mail.ru',
-  :password => ENV['MAIL_PASSWORD']
+  :password => ENV['MAIL_PASSWORD'],
+  :enable_starttls_auto => true
 }
